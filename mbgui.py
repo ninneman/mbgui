328675cfeb85bbc7965ed99077c5a3672d8a7d3f
Jason Ninneman <jsninneman@alaska.edu>
2014-11-30 13:47:46 -0900

    Add COPYING file. Tweak copyright/license message in mbgui.py.

--- mbgui.py
+++ mbgui.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 
-# MB-System file converter
-# A GUI front-end to a subset of MB-System functionality.
-# Copyright 2014, Jason S. Ninneman and Kelly Monteleone
-# <license info here>
+# mbgui: a GUI front-end to a subset of MB-System functionality.
+
+# <copyright info here>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import tkinter as tk
 import tkinter.filedialog as tkfd
