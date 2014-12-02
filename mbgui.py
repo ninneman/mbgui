145ed6e5b4e7c2f505fdbc1cd79654b7d5bede42
Jason S. Ninneman <jsn@mbar.us>
2014-12-01 21:00:39 -0900

    Getting closer to working commands.

--- mbgui.py
+++ mbgui.py
@@ -17,14 +17,20 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+mbs_bin_dir = "/home/ninneman/Documents/freelance/mbsystem-5.4.2209/src/utilities"
+
 import tkinter as tk
 import tkinter.filedialog as tkfd
+import os
+import subprocess
+import tempfile
 
 class App(tk.Frame):
     def __init__(self, master = None):
         tk.Frame.__init__(self, master)
         self.infile = tk.Entry(self)
         self.outdir = tk.Entry(self)
+        self.input_selectors = None
         self.outtypes = {
             "xyz": tk.IntVar(),
             "ascii": tk.IntVar(),
@@ -46,19 +52,30 @@ class App(tk.Frame):
 
     def convert(self):
         """Convert the input file to the output type(s)."""
-        print("converting: {0}".format(self.infile.get()))
-        print("destination: {0}".format(self.outdir.get()))
-        print("output types: {0}".format(self.get_type_states()))
+
+        mbdatalist = os.path.join(mbs_bin_dir, "mbdatalist")
+        mblist = os.path.join(mbs_bin_dir, "mblist")
+        for selector in self.input_selectors:
+            outfile_base, _ = os.path.splitext(os.path.basename(selector))
+            outfile_full_base = os.path.join(self.outdir.get(), outfile_base)
+            datalist = outfile_full_base + ".datalist"
+            final_output = outfile_full_base + ".txt"
+            print("converting {0} to {1}".format(selector, final_output))
+            subprocess.call([mbdatalist, "-F", "0", "-I", selector, ">", datalist,])
+            subprocess.call([mbdatalist, "-F", "-1", "-I", datalist, "-N",])
+            subprocess.call([mblist, "-F", "-1", "-I", datalist, "-D2", "-X", final_output,])
+            
         return None
 
-    def get_input_file(self):
-        """Pop up the 'open file' dialog, and assign whatever's picked to the input file entry box."""
+    def get_input_files(self):
+        """Pop up the 'open file' dialog, assign whatever's picked to the input file entry box, and set an attribute with an iterable of the file selectors."""
         options = {
             "title": "Select file",
         }
-        selector = tkfd.askopenfilename(**options)
+        selectors = tkfd.askopenfilenames(**options)
+        self.input_selectors = selectors
         self.infile.delete(0, tk.END)
-        self.infile.insert(0, selector)
+        self.infile.insert(0, selectors)
         return None
 
     def get_output_dir(self):
@@ -75,7 +92,7 @@ class App(tk.Frame):
         """Lay out the various widgets."""
         tk.Label(self, text = "Input file:").grid(row = 0)
         self.infile.grid(row = 1, column = 0)
-        tk.Button(self, text = "Browse...", command = self.get_input_file).grid(row = 1, column = 1)
+        tk.Button(self, text = "Browse...", command = self.get_input_files).grid(row = 1, column = 1)
         tk.Label(self, text = "Output directory:").grid(row = 2)
         self.outdir.grid(row = 3, column = 0)
         tk.Button(self, text = "Browse...", command = self.get_output_dir).grid(row = 3, column = 1)
